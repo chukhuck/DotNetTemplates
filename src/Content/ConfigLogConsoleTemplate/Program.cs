6021302f423a0c75e617942c6b284389fe9759cd
Кузнецов Александр <aka.kuznec@ya.ru>
2020-12-15 21:07:17 +0300

    Initial commit

---
+++ src/Content/ConfigLogConsoleTemplate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+
+namespace ConfigLogConsoleTemplate
+{
+    class Program
+    {
+        static public IHost host;
+        static Task Main(string[] args)
+        {
+            try
+            {
+                host = CreateHostBuilder(args)
+                        .Build();   
+
+                Log.Information("Hello World!");                
+
+                return host.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return Task.FromException(ex);
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        
+        }
+
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseEnvironment("Development")
+                .UseSerilog((hostingContext, services, loggerConfiguration) => loggerConfiguration
+                    .ReadFrom.Configuration(hostingContext.Configuration)
+                    .Enrich.FromLogContext());
+    }
+}
